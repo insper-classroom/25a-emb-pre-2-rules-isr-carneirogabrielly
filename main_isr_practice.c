e3d60172ad86e54ff095814a5dedcb041aa32456
Gabrielly <gabriellycs@al.insper.edu.br>
2025-03-12 10:54:44 -0300

    main isr correçoes

--- main_isr_practice.c
+++ main_isr_practice.c
@@ -6,11 +6,23 @@ const int BTN_PIN_R = 28;
 
 // ERRO: cppcheck unitvar
 // ERRO: addon IsrVarVolatile
-int btn_flag;
+volatile int btn_flag;
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
+  if (events == 0x4) { // fall edge    
+    btn_flag = 1;
+  }
+}
 
+int main() {
+  stdio_init_all();
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
+  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
+                                     &btn_callback);
+
+  if (btn_flag){
     // ERRO: addon IsrPrintf
     printf("btn pressed \n");
 
@@ -25,19 +37,9 @@ void btn_callback(uint gpio, uint32_t events) {
 
     // ERRO: addon IsrNoDelay
     sleep_ms(1);
-    btn_flag = 1;
-  }
-}
-
-int main() {
-  stdio_init_all();
-  gpio_init(BTN_PIN_R);
-  gpio_set_dir(BTN_PIN_R, GPIO_IN);
-  gpio_pull_up(BTN_PIN_R);
-  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                     &btn_callback);
-
-  volatile int capture_flag = 0;
+  };
+  
+  int capture_flag = 0;
   int a;
   while (1) {
     if (btn_flag) {
